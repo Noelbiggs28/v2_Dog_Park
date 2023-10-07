e348c11be4aae06c0acf56a9eabd1d3a6d0e2e55
Noelbiggs28 <noelbiggs28@gmail.com>
2023-10-07 11:14:06 -1000

    fixed settings in setup.py

--- dog_park_backend/setup_data.py
+++ dog_park_backend/setup_data.py
@@ -1,5 +1,12 @@
 # exec(open("./setup_data.py").read())
+import os
+import django
 
+# Set the DJANGO_SETTINGS_MODULE environment variable
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dog_park_proj.settings")
+
+# Initialize Django
+django.setup()
 from park_app.models import Dog_Park
 
 Dog_Park.objects.create(dog_park_name="Mililani")
