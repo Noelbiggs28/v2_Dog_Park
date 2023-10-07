d205a16d4ff38da668239b0677c55a8255553ceb
Noelbiggs28 <noelbiggs28@gmail.com>
2023-10-07 11:04:18 -1000

    fixed locatoin of dog park

--- dog_park_backend/setup_data.py
+++ dog_park_backend/setup_data.py
@@ -1,7 +1,7 @@
 # exec(open("./setup_data.py").read())
 
-from dog_park_backend.park_app.models import Dog_Park
+from park_app.models import Dog_Park
 
-Dog_Park.objects.create(dog_park_name="Wood Shavings")
-Dog_Park.objects.create(dog_park_name="Apple Muffins")
-Dog_Park.objects.create(dog_park_name="Trails")
+Dog_Park.objects.create(dog_park_name="Mililani")
+Dog_Park.objects.create(dog_park_name="Waipahu")
+Dog_Park.objects.create(dog_park_name="Honolulu")
